bcad37bb322e1093f634d446a35503756db53624
Shipu Ahamed <shipu@10minuteschool.com>
2023-08-27 01:54:11 +0600

    update route name

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Shipu\WebInstaller\Livewire\Installer;
 
-Route::get('/', function () {
-    return view('installer::success');
-})->name('installer.success');
 
 Route::get('installer', Installer::class)->name('installer')
     ->middleware(['web']);
+
+Route::get('/installed', function () {
+    return view('installer::success');
+})->name('installer.success');
