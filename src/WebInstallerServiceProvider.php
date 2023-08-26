3e6c3543c71dfd323f4ec626665d3b800526e22f
Shipu Ahamed <shipu@10minuteschool.com>
2023-08-27 01:59:55 +0600

    add alias middleware

--- src/WebInstallerServiceProvider.php
+++ src/WebInstallerServiceProvider.php
@@ -4,6 +4,7 @@ namespace Shipu\WebInstaller;
 
 use Livewire\Livewire;
 use Shipu\WebInstaller\Livewire\Installer;
+use Shipu\WebInstaller\Middleware\RedirectIfNotInstalled;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -12,6 +13,8 @@ class WebInstallerServiceProvider extends PackageServiceProvider
     public function configurePackage(Package $package): void
     {
         Livewire::component('web-installer', Installer::class);
+
+        $this->app['router']->aliasMiddleware('redirect.if.not.installed', RedirectIfNotInstalled::class);
 
         $package->name('web-installer')
             ->hasAssets()
